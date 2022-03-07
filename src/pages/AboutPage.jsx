e67642bc7d8d7d6595ecb67a1bd7e5f051705b21
Meen Tycoon <meenphilip@gmail.com>
2022-03-07 13:24:16 +0300

    created Routes and links

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import Card from "../components/shared/Card";
+
+const AboutPage = () => {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a React app to leave feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+        <p>
+          <Link
+            to="/"
+            // to={{
+            //   pathname: "/about",
+            //   search: "?sort=name",
+            //   hash: "#helloworld",
+            // }}
+          >
+            Back To Home
+          </Link>
+        </p>
+      </div>
+    </Card>
+  );
+};
+
+export default AboutPage;
